a031fe39e5c00e149e2829f0668d97ba492b77df
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-10-24 14:14:53 -0400

    add cat

---
+++ CatCarousel/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// $('.thumbnail').on('click', e => {
+//     $('.hero').attr("src", "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-65/cat2.jpg"));
+//     console.log(e.target);
+//     console.log('i am here');
+// };
+
+function catClicks() {
+    $('.thumbnail').on('click', e => {
+        let thumbnailSrc = $(e.currentTarget).find('img').attr('src');
+        let thumbnailAlt = $(e.currentTarget).find('img').attr('alt');
+        console.log(thumbnailAlt);
+      $('.hero img').attr('src', thumbnailSrc).attr('alt', thumbnailAlt);
+    });
+   };
+
+   $(catClicks);
