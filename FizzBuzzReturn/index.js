e3b0075312243f1d98b8d5574870a4afb514f94e
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-10-24 21:26:54 -0400

    finished fizzbuzz drill

--- FizzBuzzReturn/index.js
+++ FizzBuzzReturn/index.js
@@ -1,9 +1,39 @@
-function insertNumber() {
+'use strict'
 
-    $('#number-chooser').submit(e => {
-        e.preventDefault();
-        // let numberInput = 
-        // $('.js-results').
-    });
+function fizzBuzzReturn() {
 
+  $('#number-chooser').submit( e => {
+
+    e.preventDefault();
+    $(".js-results").empty();
+
+    const numInput = $(e.currentTarget).find('input').val();
+
+    
+    
+    const fullCount = [];
+
+    for (let i = 1; i <= numInput; i++){
+      if (i % 15 === 0) {
+        fullCount.push(
+          $('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));
+      }
+      else if (i % 5 === 0) {
+        fullCount.push(
+          $('<div class="fizz-buzz-item buzz"><span>buzz</span></div>')); 
+      }
+      else if (i % 3 === 0) {
+        fullCount.push(
+          $('<div class="fizz-buzz-item fizz"><span>fizz</span></div>')); 
+      }
+      else {
+        fullCount.push(
+          $(`<div class="fizz-buzz-item"><span>${i}</span></div>`)); 
+      }
+    }
+    $(".js-results").append(fullCount);
+
+  });
 }
+
+$(fizzBuzzReturn());
